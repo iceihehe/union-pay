04e9ebc8d8aa28e148b081b161d9f3a46e3437f6
jiabing <jiabing@wolongdata.com>
2016-07-05 14:38:31 +0800

    添加了真正银联的签名和验签

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ v1.0 添加了请求参数签名方法和返回参数验证签名方法
 
 ### 用法
 
+#### chinapay
+
 ##### 给请求参数签名，实例化四个参数依次是商户ID，交易证书路径（pfx文件），交易证书密码，验签证书路径
 ```python
 >>>from basic import ChinaPay
@@ -20,6 +22,26 @@ v1.0 添加了请求参数签名方法和返回参数验证签名方法
 ##### 对银联异步通知请求验证签名
 ```python
 >>>data = {"TranType": "0001", "TranDate": "20160602", "MerId": "000001605260645", "BusiType": "0001", "AcqDate": "20160602", "CurryNo": "CNY", "TranTime": "023529", "CompleteDate": "20160602", "BankInstNo": "700000000000023", "Version": "20140728", "AcqSeqId": "0000000005841781", "Signature": "THHDUP2mlOraPcl6MY3ngC6%2FdWUSn4LFEVDzjS1BIpLsQxhsXe5Jao5V%2BzEzckpROQ8%2By4VzqwRlzphRABBN%2FyowGhJc%2FxJ9viTgE%2BGn%2Be6cErfkUxdH52Dz5G8jdvUgTR1g0SgZXBBvo%2BCmwk7scGkMYnvF2W9%2F0kvPTqBDGqQ%3D", "CompleteTime": "103237", "MerOrderNo": "770913", "OrderAmt": "1", "OrderStatus": "0000"}
+>>>verify = cp.verify(data)
+>>>verify
+True
+```
+
+#### unionpay
+
+##### 给请求参数签名，实例化四个参数依次是商户ID，交易证书路径（pfx文件），交易证书密码，验签证书路径
+```python
+>>>from basic import UnionPay
+>>>cp = UnionPay('merchant_id', 'path/to/transaction/certificate', 'transaction_certificate_password', 'path/to/verify/certificate')
+>>>data = {"Version": 20140728, "OrderAmt": 1, "TranDate": 11111111, "BusiType": "0001", "MerBgUrl": "http://zfb.cdecube.com/", "MerPageUrl": "http://zfb.cdecube.com/", "MerOrderNo": 111, "TranTime": 111111, "MerId": "000001605260645"}
+>>>data_signed = cp.sign(data)
+>>>data_signed
+{'TranDate': 11111111, 'BusiType': '0001', 'certId': '40220995861346480087409489142384722381', 'TranTime': 111111, 'Version': 20140728, 'OrderAmt': 1, 'signature': 'BWZv3S4kTbuAisX1/aibDg7AfTjWDG2XLIGrSyQ96iAnYRW+yVfnyUL1/0JCcBxl/XFpYKxhBwFTR6sAYEVP9HQsOPV5f62XC9/+vvsnO3RDj/41Y9mBnAOCKsa07aJhommkWhYKKeD9bxFL91/jcHo6kag+1ips5jA8dqVjc9Q=', 'MerBgUrl': 'http://zfb.cdecube.com/', 'MerPageUrl': 'http://zfb.cdecube.com/', 'MerOrderNo': 111, 'MerId': '000001605260645'}
+```
+
+##### 对银联异步通知请求验证签名
+```python
+>>>data = {"accessType":"0","bizType":"000201","certId":"68759585097","currencyCode":"156","encoding":"UTF-8","merId":"802310053110831","orderId":"4036074942","queryId":"201607041629538091698","respCode":"00","respMsg":"success","settleAmt":"1","settleCurrencyCode":"156","settleDate":"0704","signMethod":"01","signature":"P9ZwFBs5dCC3cHk4XULFfrk0I4DaDtdTufDRqwoh0VeM9F2I4IqrZs7yMKz8DO3KdJ8GONTHhEHUjrqq+rvVR2p+Hnusb1az/Lxc1X198d4uWekB4ieEhoSWXF0zY3EP8HARtPdcvBb/hR5aWUfOZeI/x1RBPCqReBBuVgMNUa2s5dWbESJYneu57djEaNmPrwiuTecdVrLNUNxnH8rRjvLJI0cJvTgcio9/1HjSCH+45bKKZ003PRhZY+VgDtR8xqY8/xtUfrvqotqF6TDuMPS4WN7sBNDBSHsFDaztv5htwJ9Z6H2Lnz5lgtEStlyu/VPCWYQ8XkcvO1ZcQJKbBA==","traceNo":"809169","traceTime":"0704162953","txnAmt":"1","txnSubType":"01","txnTime":"20160704162953","txnType":"01","version":"5.0.0"}
 >>>verify = cp.verify(data)
 >>>verify
 True
--- basic.py
+++ basic.py
@@ -3,6 +3,7 @@
 import OpenSSL.crypto
 import base64
 import urllib
+import hashlib
 
 
 class ChinaPay(object):
@@ -12,7 +13,7 @@ class ChinaPay(object):
         :param MerId: 商户号
         :param pfx_path: 交易证书路径
         :param pfx_pwd: 交易证书密码
-        :param cer_path: 延签证书路径
+        :param cer_path: 验签证书路径
         """
 
         self.MerId = MerId
@@ -27,7 +28,7 @@ class ChinaPay(object):
         return pfx
 
     def _load_cer(self, cer_path):
-        """载入延签证书"""
+        """载入验签证书"""
 
         with open(cer_path, 'rt') as f:
             cer = OpenSSL.crypto.load_certificate(
@@ -41,18 +42,19 @@ class ChinaPay(object):
         keys.sort()
         return '&'.join(['%s=%s' % (key, data[key]) for key in keys])
 
-    def sign(self, data, alg='sha512'):
+    def sign(self, data):
         """签名
         :param data: 待签名的dictionary
         :param alg: 签名算法
         """
 
+        alg = 'sha512'
         sign_raw_str = self._get_sign_str(data)
 
         signature = OpenSSL.crypto.sign(self.pfx._pkey, sign_raw_str, alg)
         return base64.b64encode(signature)
 
-    def verify(self, data, alg='sha512', is_bg=False):
+    def verify(self, data, is_bg=False):
         """验签
         :param data: 银联返回的已签名的dictionary
         :param alg: 签名算法
@@ -64,11 +66,81 @@ class ChinaPay(object):
             for k, v in data.items():
                 data[k] = urllib.unquote_plus(v)
 
+        alg = 'sha512'
         signature = base64.b64decode(data.pop('Signature'))
         sign_raw_str = self._get_sign_str(data)
 
         try:
             OpenSSL.crypto.verify(self.cer, signature, sign_raw_str, alg)
+            return True
+        except OpenSSL.crypto.Error:
+            return False
+
+
+class UnionPay(object):
+
+    def __init__(self, MerId, pfx_path, pfx_pwd, cer_path):
+        """
+        :param MerId: 商户号
+        :param pfx_path: 交易证书路径
+        :param pfx_pwd: 交易证书密码
+        :param cer_path: 验签证书路径
+        """
+
+        self.MerId = MerId
+        self.pfx = self._load_pfx(pfx_path, pfx_pwd)
+        self.cer = self._load_cer(cer_path)
+        self.certId = self.pfx._cert.get_serial_number()
+
+    def _load_pfx(self, pfx_path, pfx_pwd):
+        """载入交易证书"""
+
+        with open(pfx_path, 'rb') as f:
+            pfx = OpenSSL.crypto.load_pkcs12(f.read(), pfx_pwd)
+        return pfx
+
+    def _load_cer(self, cer_path):
+        """载入验签证书"""
+
+        with open(cer_path, 'rt') as f:
+            cer = OpenSSL.crypto.load_certificate(
+                OpenSSL.crypto.FILETYPE_PEM, f.read())
+        return cer
+
+    def _get_sign_str(self, data):
+        """将待签名dictionary转换成str"""
+
+        keys = data.keys()
+        keys.sort()
+        return '&'.join(['%s=%s' % (key, data[key]) for key in keys])
+
+    def sign(self, data):
+        """签名
+        :param data: 待签名的dictionary
+        """
+
+        data.update({'certId': str(self.certId)})
+
+        alg = 'sha1'
+        sign_raw_str = self._get_sign_str(data)
+        sign_raw_str_sha1 = hashlib.sha1(sign_raw_str).hexdigest()
+
+        signature = OpenSSL.crypto.sign(self.pfx._pkey, sign_raw_str_sha1, alg)
+        data.update({'signature': base64.b64encode(signature)})
+        return data
+
+    def verify(self, data):
+        """验签
+        :param data: 银联返回的已签名的dictionary
+        """
+
+        alg = 'sha1'
+        signature = base64.b64decode(data.pop('signature'))
+        sign_raw_str = self._get_sign_str(data)
+        sign_raw_str_sha1 = hashlib.sha1(sign_raw_str).hexdigest()
+
+        try:
+            OpenSSL.crypto.verify(self.cer, signature, sign_raw_str_sha1, alg)
             return True
         except OpenSSL.crypto.Error:
             return False
